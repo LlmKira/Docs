166e6783ca7bc892aa6eee8c06e042f908a71eda
sudoskys <sudoskys@users.noreply.github.com>
2023-10-29 03:29:55 +0000

    deploy: 9815f2ee56090675b0b8c3cdb032e1c2029c01ce

---
+++ assets/dev_arch.md.76b01a8a.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a}from"./chunks/framework.63944a88.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"dev/arch.md","filePath":"dev/arch.md","lastUpdated":1697964564000}'),r={name:"dev/arch.md"};function c(o,s,d,n,p,_){return t(),a("div")}const f=e(r,[["render",c]]);export{m as __pageData,f as default};
---
+++ assets/en_dev_client.md.ad2c4fe0.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a}from"./chunks/framework.63944a88.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"en/dev/client.md","filePath":"en/dev/client.md","lastUpdated":1697964564000}'),n={name:"en/dev/client.md"};function c(r,o,s,d,i,l){return t(),a("div")}const m=e(n,[["render",c]]);export{_ as __pageData,m as default};
